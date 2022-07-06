7021a05d0800eecb43407d49651f67bb26c6a9aa
Andrew <grozzy09@gmail.com>
2022-07-06 16:57:49 -0400

    Added public class

--- Program.cs
+++ Program.cs
@@ -4,6 +4,25 @@
 // Console.WriteLine is from the system library and allows us access to write a line of text to the console.
 // (__) parameters. These say we are sending Console.WriteLine whtever is insisde the ().
 // "" denote a string literal.
-// ; Endline character;
+// ; Endline character.
 
-Console.WriteLine("Hello, World!");
+public class program {
+    public static void Main() {
+        Console.WriteLine();
+        Console.Write(); //Writes to the console, but it does not automatically end the line
+        Console.ReadLine(); //Reads from the console.
+    }
+}
+
+// EVERYTHING IS AN OBJECT
+
+// An object is ...
+// a real world entity with state and behaviour.
+
+// Data Types
+
+int x = 5; //integer
+float y = 5.5;
+double z = 5.5;
+long a = 5;
+short b = 5;
