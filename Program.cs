9ce0d044838aef17e86533f5f1eec2627b871684
Andrew <grozzy09@gmail.com>
2022-07-07 11:38:52 -0400

    C Sharp Concepts

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
 // "" denote a string literal.
 // ; Endline character.
 
+using System;
 public class program {
     public static void Main() {
         Console.WriteLine("Hello");
@@ -19,10 +20,41 @@ public class program {
 // An object is ...
 // a real world entity with state and behaviour.
 
-// Data Types
 
-int x = 5; //integer
-float y = 5.5;
-double z = 5.5;
-long a = 5;
-short b = 5;
+namespace CSharpConcepts {
+
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World");
+
+                        // Data Types
+
+            int x = 5; //integer
+            float y = 5.5;
+            double z = 5.5;
+            long a = 5;
+            short b = 5;
+
+            int newint = 4;
+
+            switch(newint) {
+                case 1:
+                    Console.WriteLine("1");
+                    break;
+                case 2:
+                    Console.WriteLine("2");
+                    break;
+                case 3:
+                    Console.WriteLine("3");
+                    break;
+                case 4:
+                    Console.WriteLine("4");
+                    break;
+                default:
+                    Console.WriteLine("default");
+                    break;
+            }
+
+        }
+    }
+}
